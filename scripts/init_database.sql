0f01a5a9efe19b9318f98e17fa0fcd7429ebbdbc
PhucDaThu <vohoangphuc131204@gmail.com>
2026-01-21 18:44:05 +0700

    Update and rename scripts/init/init_database.sql to scripts/init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,12 +4,37 @@ tạo database tên DataWarehouse
 ====================================
 Mục đích của scripts: 
   Tạo ra database mang tên "DataWarehouse", đầu tiên ta kiểm tra xem đã tồn tại database đó chưa. 
-  Nếu rồi thì xóa và tạo lại database đó. Sau khi tạo xong hãy kết nối tới Database DataWarehouse và chạy tiếp lệnh
-  trong init_schemas.sql để tạo ra 3 schemas lần lượt mang tên bronze, silver, gold.
+  Nếu rồi thì xóa và tạo lại database đó. Sau đó tạo ra 3 schemas lần lượt mang tên bronze, silver, gold.
 Lưu ý:
   Sử dụng scripts này sẽ xóa đi tất cả dữ liệu của database: "DataWarehouse" đã tồn tại.
   Hãy đảm bảo bạn đã backup mọi dữ liệu trước khi chạy scripts này.
-  Hãy chạy từng câu lệnh một vì trong PgAdmin không chạy được lệnh DROP và CREATE trên cùng một khối
 */
-DROP Database IF EXISTS DataWarehouse;
-CREATE Database DataWarehouse;
+USE master
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name= 'DataWarehouse')
+BEGIN
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
+
+
+
+
